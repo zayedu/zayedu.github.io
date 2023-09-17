be4ba2a0d34c89ed12c2d0b2e19c391a7ace5c09
zayedu <umerzayed1@gmail.com>
2023-09-17 01:12:05 -0400

    fixed some text

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -11,7 +11,7 @@ const Qualification = () => {
   return (
     <section className="qualification section">
       <h2 className="section__title">Qualification</h2>
-      <span className="section__subtitle">My personel journey</span>
+      <span className="section__subtitle">My personal journey</span>
 
       <div className="qualification__container container">
         <div className="qualification__tabs">
