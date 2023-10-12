d64b58b0edfae3e4c57dbedd952c09bc7b7dcde5
zayedu <umerzayed1@gmail.com>
2023-10-12 18:07:50 -0400

    fixing bug

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,7 +2,7 @@ import Work1 from "../../assets/work1.jpg";
 import Work2 from "../../assets/work2.jpg";
 import Work3 from "../../assets/work3.jpg";
 import Work4 from "../../assets/SignLanguageDetection_Demo.jpg";
-import Work5 from "../../assets/FinanceBotDemo.jpg";
+import Work5 from "../../assets/chatbotdemo.jpg";
 export const projectsData = [
   {
     id: 1,
