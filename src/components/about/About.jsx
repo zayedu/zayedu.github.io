403c97477004db1f1ddacf01ec6ae1024288d5d1
zayedu <umerzayed1@gmail.com>
2024-01-11 14:35:07 -0500

    Updated Resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../Resume-ZayedU.pdf";
+import CV from "../assets/Resume-ZayedU.pdf";
 import Info from "./Info";
 
 const About = () => {
