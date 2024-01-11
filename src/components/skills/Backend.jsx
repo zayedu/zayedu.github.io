6004434dbe02e22e70f8f1defb2034bab6232d1e
zayedu <umerzayed1@gmail.com>
2024-01-11 14:54:06 -0500

    Updated Skills🧠

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -81,6 +81,14 @@ const Backend = () => {
             <i className="bx bx-badge-check"></i>
 
             <div>
+              <h3 className="skills__name">Kanban</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
               <h3 className="skills__name">Agile Methodologies</h3>
               <span className="skills__level">Intermediate</span>
             </div>
